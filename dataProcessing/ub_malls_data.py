36ab31b976e77789bf649b3ce74e74ef262b7dd1
oath2yangmen <2278777286@qq.com>
2017-10-27 22:42:56 -0500

    Update ub_malls_data.py

--- dataProcessing/ub_malls_data.py
+++ dataProcessing/ub_malls_data.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 27 14:39:38 2017
-
 @author: linwanying
 """
 
@@ -10,24 +9,30 @@ from pandas import DataFrame
 import pickle
 #from sklearn.cross_validation import train_test_split
 
+#根据自己的目录修改localpath之后使用
+localpath = "/"
+
 # 把每个mall的所有数据分开存储
-shops=pd.read_csv('C:/files/locationtheshop/ccf_first_round_shop_info.csv')
-ub = pd.read_csv('C:/files/locationtheshop/ccf_first_round_user_shop_behavior.csv')
+shops=pd.read_csv(localpath+'ccf_first_round_shop_info.csv')
+ub = pd.read_csv(localpath+'ccf_first_round_user_shop_behavior.csv')
 
 newshop=shops[['mall_id','shop_id']]
 newub=pd.merge(ub, newshop, on='shop_id', how='left', sort=False)
 
-newub.to_csv('C:/files/locationtheshop/newub.csv')
-test = pd.read_csv('C:/files/locationtheshop/evaluation_public.csv')
+newub.to_csv(localpath+'newub.csv')
+test = pd.read_csv(localpath+'evaluation_public.csv')
 
 # 保存所有商场信息
 malls=shops['mall_id'].unique()
-f=open('C:/files/locationtheshop/malls.bin','wb')
+f=open(localpath+'malls.bin','wb')
 pickle.dump(malls,f)
 f.close()
 
+i = len(malls)
 for m in malls:
+    i -= 1
+    print(i)
     malla=newub[newub.mall_id == m]
     malla_test=test[test.mall_id == m]
-    malla.to_csv('C:/files/locationtheshop/malls/'+m+'.csv')
-    malla_test.to_csv('C:/files/locationtheshop/malls/test_'+m+'.csv')
+    malla.to_csv(localpath+'malls/'+m+'.csv')
+    malla_test.to_csv(localpath+'malls/test_'+m+'.csv')
