9b2e5ddf97e201b4634789faae6012e2ce29bd7f
oath2yangmen <2278777286@qq.com>
2017-10-27 22:44:36 -0500

    Update mall_wifis.py

--- dataProcessing/mall_wifis.py
+++ dataProcessing/mall_wifis.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 27 14:42:35 2017
-
 @author: linwanying
 """
 import pandas as pd 
 import pickle
 
-f=open('C:/files/locationtheshop/malls.bin','rb')
+localpath = "/"
+
+f=open(localpath+'malls.bin','rb')
 malls=pickle.load(f)
 
 # 取出每个mall的训练集和评估集的所有wifi集合的交集
+j = len(malls)
 for mall in malls:
-    ub = pd.read_csv('C:/files/locationtheshop/malls/'+mall+'.csv')
-    test = pd.read_csv('C:/files/locationtheshop/malls/test_'+mall+'.csv')
+    j = j - 1
+    print(j)
+    ub = pd.read_csv(localpath+'malls/'+mall+'.csv')
+    test = pd.read_csv(localpath+'malls/test_'+mall+'.csv')
     ubwifi = ub.wifi_infos.apply(lambda x: list(map(lambda y: y.split('|')[0], x.split(';'))))
     testwifi = test.wifi_infos.apply(lambda x: list(map(lambda y: y.split('|')[0], x.split(';'))))
     wifi1=[]
@@ -23,5 +27,5 @@ for mall in malls:
     for i in testwifi:
         wifi2 = wifi2 + i
     wifis=set(wifi1)&set(wifi2)
-    f1 = open('C:/files/locationtheshop/malls/wifis_'+mall+'.bin','wb')
+    f1 = open(localpath+'malls/wifis_'+mall+'.bin','wb')
     pickle.dump(wifis,f1)
